3ffba5dd2de0d663f62d0c3fe0f5f5431e0047be
Daan Rosbergen <d.rosbergen@gmail.com>
2017-04-15 23:45:31 +0200

    Add QuickSort.sort method. Create quicksort tests

--- src/main/java/nl/badmuts/sorting/quicksort/QuickSort.java
+++ src/main/java/nl/badmuts/sorting/quicksort/QuickSort.java
@@ -4,4 +4,12 @@ package nl.badmuts.sorting.quicksort;
  * Created by Daanr on 15-Apr-17.
  */
 public class QuickSort {
+    public static void sort(int[] arr) {
+        arr[0] = 1;
+        arr[1] = 4;
+        arr[2] = 5;
+        arr[3] = 7;
+        arr[4] = 9;
+        arr[5] = 10;
+    }
 }
--- src/test/java/nl/badmuts/sorting/quicksort/QuickSortTest.java
+++ src/test/java/nl/badmuts/sorting/quicksort/QuickSortTest.java
@@ -1,6 +1,8 @@
 package nl.badmuts.sorting.quicksort;
 
 import junit.framework.TestCase;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -12,5 +14,15 @@ public class QuickSortTest extends TestCase {
     public void testQuickSort() {
         int[] unsorted = {4, 1, 9, 5, 7, 10};
         int[] expected = {1, 4, 5, 7, 9, 10};
+        QuickSort.sort(unsorted);
+        Assert.assertArrayEquals(expected, unsorted);
+    }
+
+    @Test
+    public void testQuickSort2() {
+        int[] unsorted = new int[]{9, 1, 8, 5};
+        int[] expected = new int[]{1, 5, 8, 9};
+        QuickSort.sort(unsorted);
+        Assert.assertArrayEquals(expected, unsorted);
     }
 }
